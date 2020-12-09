e87e20e6368619ecd0707cd2afbbfa5453a9f290
Smekalin Sergei <crypto.mnr@gmail.com>
2020-12-09 23:11:45 +0200

    Набросал скелет + плюшки

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,10 +1,18 @@
 const refs = {
-  openModalBtn: document.querySelector('[data-modal-open]'),
+  openModalSearchBtn: document.querySelector('[data-modal-search]'),
+  openModalSearchMobileBtn: document.querySelector('[data-modal-search-mobile]'),
+  openModalLoginBtn: document.querySelector('[data-modal-login]'),
+  openModalCreateBtn: document.querySelector('[data-modal-create]'),
+  openModalCreateMobileBtn: document.querySelector('[data-modal-create-mobile]'),
   closeModalBtn: document.querySelector('[data-modal-close]'),
   modal: document.querySelector('[data-modal]'),
 };
 
-// refs.openModalBtn.addEventListener('click', toggleModal);
+refs.openModalSearchBtn.addEventListener('click', toggleModal);
+refs.openModalSearchMobileBtn.addEventListener('click', toggleModal);
+refs.openModalLoginBtn.addEventListener('click', toggleModal);
+refs.openModalCreateBtn.addEventListener('click', toggleModal);
+refs.openModalCreateMobileBtn.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
 
 function toggleModal() {
