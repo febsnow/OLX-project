a3bf75c2f621ad07d524ca297a7a1e1273170432
Smekalin Sergei <crypto.mnr@gmail.com>
2020-12-18 14:24:58 +0200

    много всего

---
+++ src/js/resetFilter.js
@@ -0,0 +1,17 @@
+import { getProductList } from './vi-getProductsGallery';
+export const resetBtn = document.querySelector('[data-action="reset"]');
+
+export function resetFilter() {
+  const listOfFilters = document.querySelectorAll('[data-action="filter"]');
+  function resetFilter(items) {
+    items.forEach(item => {
+      if (item.hasAttribute('disabled') && item.classList.contains('active')) {
+        item.classList.remove('active');
+        item.removeAttribute('disabled');
+        history.pushState({ page: 'main' }, null, 'index.html');
+        // getProductList(); //для включения нужен шаблон галереи
+      }
+    });
+  }
+  resetFilter(listOfFilters);
+}
